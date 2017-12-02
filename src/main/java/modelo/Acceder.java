295d1d3550b4c4dcbfb37d1e5eb379016a7902d9
NetsonNet <31219039+NetsonNet@users.noreply.github.com>
2017-12-02 10:53:30 -0600

    Add files via upload

---
+++ src/main/java/modelo/Acceder.java
@@ -0,0 +1,15 @@
+package modelo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.dev4u.hv.guia7.R;
+
+public class Acceder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceder);
+    }
+}
